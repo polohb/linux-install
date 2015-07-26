5742472c0df409c9efe47e0bd7eabd1d5efdab21
polohb <polohb@gmail.com>
2015-07-26 10:31:28 +0200

    assume yes to apt

--- base-install.sh
+++ base-install.sh
@@ -8,23 +8,23 @@
 
 
 function fn_update {
-  apt-get udpate
-  apt-get upgrade
-  apt-get dist-upgrade
+  apt-get udpate -y
+  apt-get upgrade -y
+  apt-get dist-upgrade -y
 
 }
 
 # essential software
 function fn_install_essentials {
-  apt-get install curl git vim screen
+  apt-get -y install curl git vim screen
 }
 
 # google-chrome install
 function fn_install_chrome {
   wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
   sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-  apt-get update
-  apt-get install google-chrome-stable
+  apt-get update -y
+  apt-get install -y google-chrome-stable
 }
 
 
