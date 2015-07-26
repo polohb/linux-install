2dcd37bd0022ebef8e7faddeec4f5411d7d67949
polohb <polohb@gmail.com>
2015-07-26 10:19:51 +0200

    add update part in base-install

--- base-install.sh
+++ base-install.sh
@@ -7,7 +7,12 @@
 #
 
 
+function fn_update {
+  apt-get udpate
+  apt-get upgrade
+  apt-get dist-upgrade
 
+}
 
 # essential software
 function fn_install_essentials {
@@ -20,41 +25,4 @@ function fn_install_chrome {
   sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
   apt-get update
   apt-get install google-chrome-stable
-}
-
-
-
-# Get .ssh folder :)
-function fn_user_get_ssh_files {
-  # you know how to do it ;)
-  # never put private key files in a public repo :o
-  # So don't do anything via public scripts
-}
-
-# Get dotfiles
-function fn_user_install_dotfiles {
-  cd
-  git clone https://github.com/polohb/dotfiles.git
-  cd dotfiles/scripts
-  bash install.sh
-}
-
-# Configure git
-function fn_user_configure_git {
-  git config --global user.name "polohb"
-  git config --global user.email "polohb@gmail.com"
-}
-
-
-# Getting motivational-wallpapers
-function fn_user_get_wallpaper {
-  cd ~/Images
-  git clone git@gitlab.com:polohb/motivational-wallpapers.git
-}
-
-
-funcion fn_user_clean_home{
-  cd
-  rm -rf Bureau Modèles Musique Public Vidéos
-  ln -s Téléchargements downloads
 }
