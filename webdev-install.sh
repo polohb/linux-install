d7fc636a3798bd118613ea2f34d8c1a80265f6b1
polohb <polohb@gmail.com>
2015-07-26 10:53:12 +0200

    fix sudo cmd force apt --yes fix atom synch plugin (no need to cd .atom/packages) call functions

--- webdev-install.sh
+++ webdev-install.sh
@@ -10,21 +10,21 @@
 
 # atom
 function fn_install_atom {
-  add-apt-repository ppa:webupd8team/atom
-  apt-get update
-  apt-get install atom
+  sudo add-apt-repository ppa:webupd8team/atom
+  sudo apt-get update
+  sudo apt-get -y install atom
 }
 
-# nodejs
+# nodejs and npm
 function fn_install_nodejs {
   curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
-  apt-get install --yes nodejs
+  sudo apt-get install --yes nodejs
 }
 
 
 # Meteor Install (as user)
 # this will create  a .meteor in the user home folder
-function fn_user_install_meteor {
+function fn_install_meteor {
   curl https://install.meteor.com/ | sh
 }
 
@@ -35,10 +35,14 @@ function fn_to_do {
 }
 
 # Getting atom packages
-function fn_user_get_atom_plugin {
-  cd ~/.atom/packages
+function fn_get_atom_plugin {
   apm stars --user polohb --install
 }
 
+# root install
 fn_install_nodejs
 fn_install_atom
+
+#user install
+fn_get_atom_plugin
+fn_install_meteor
