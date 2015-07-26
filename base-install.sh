6f6dd9a0376d273a5542881e625edfa638c347de
polohb <polohb@gmail.com>
2015-07-26 10:33:20 +0200

    remove a sudo action add comments (script should be run as root)

--- base-install.sh
+++ base-install.sh
@@ -5,7 +5,8 @@
 #
 # Tested on an fresh install of xubuntu 15.04 version
 #
-
+# This script should be run as root
+# 
 
 function fn_update {
   apt-get udpate -y
@@ -21,7 +22,7 @@ function fn_install_essentials {
 
 # google-chrome install
 function fn_install_chrome {
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
   sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
   apt-get update -y
   apt-get install -y google-chrome-stable
