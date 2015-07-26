25514c0b70f8dbc8bb851609347d684e51088f9a
polohb <polohb@gmail.com>
2015-07-26 10:42:13 +0200

    call function for webdev-isntall fix function name

--- webdev-install.sh
+++ webdev-install.sh
@@ -9,14 +9,14 @@
 
 
 # atom
-function fn_root_install_atom {
+function fn_install_atom {
   add-apt-repository ppa:webupd8team/atom
   apt-get update
   apt-get install atom
 }
 
 # nodejs
-function fn_root_install_nodejs {
+function fn_install_nodejs {
   curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
   apt-get install --yes nodejs
 }
@@ -28,7 +28,7 @@ function fn_user_install_meteor {
   curl https://install.meteor.com/ | sh
 }
 
-functio fn_to_do {
+function fn_to_do {
   # Need to d this to be able to use npm
   # TODO : find when
   sudo chown -R $USER ~/.npm/
@@ -39,3 +39,6 @@ function fn_user_get_atom_plugin {
   cd ~/.atom/packages
   apm stars --user polohb --install
 }
+
+fn_install_nodejs
+fn_install_atom
